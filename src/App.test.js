ba98e0cf2c0d55d1e2a9cceee1e0e8c2d0587e5a
Ryan Travitz <rtravitz@gmail.com>
2018-02-25 13:09:06 -0500

    Add testing to MoveList

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Modicum from './Modicum';
+import React from 'react'
+import Modicum from './Modicum'
+import { shallow } from 'enzyme'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+describe('Modicum', () => {
+  describe('addingASecondTime', () => {
+    it('returns true if the current number of times in the list is 1', () => {
+      const wrapper = shallow(<Modicum />)
+      wrapper.setState({times: ['firsttime']})
+      const result = wrapper.instance().addingASecondTime()
+
+      expect(result).toBe(true)
+    })
+  })
+
+  describe('thereAreTwoTimes', () => {
+    it('returns true if the current number of times in the list is 2', () => {
+      const wrapper = shallow(<Modicum />)
+      wrapper.setState({times: ['firsttime', 'secondtime']})
+      const result = wrapper.instance().thereAreTwoTimes()
+
+      expect(result).toBe(true)
+    })
+  })
+})
---
+++ src/MoveList.test.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import MoveList from './MoveList'
+
+describe('MoveList', () => {
+  const moves = [
+    {startTime: 'time1', name: 'twirly', videoId: 'abc123'},
+    {startTime: 'time2', name: 'spinny', videoId: 'xyz456'},
+  ]
+
+  const defaultProps = {
+    targetedMove: {name: 'twirly'},
+    targetMove: () => {},
+    moves 
+  }
+
+  it('renders an entire list of moves that is passed in', () => {
+    const wrapper = shallow(<MoveList {...defaultProps} />)
+    const texts = wrapper.find('li').map(node => node.text())
+
+    expect(texts).toEqual(['Name: twirly, Start time: time1, Video Id: abc123', 'Name: spinny, Start time: time2, Video Id: xyz456'])
+  })
+
+  it('highlights the active move with a pretty color', () => {
+    const wrapper = shallow(<MoveList {...defaultProps} />)
+    const styles = wrapper.find('li').map(node => node.props().style)
+
+    expect(styles[0]).toEqual({backgroundColor: 'tomato'})
+    expect(styles[1]).toEqual({})
+  })
+
+  it('fires targetMove when the move list item is clicked', () => {
+    const mockFn = jest.fn()
+    const wrapper = shallow(<MoveList {...defaultProps} targetMove={mockFn} />)
+
+    wrapper.find('li').first().simulate('click')
+    
+    expect(mockFn).toHaveBeenCalledWith({startTime: 'time1', name: 'twirly', videoId: 'abc123'})
+  })
+})
