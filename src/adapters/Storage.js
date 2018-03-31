3badd03f4c26fa8c23cc3f4f1e7d1f8c6f1da62e
Lee Saville <l.w.saville@gmail.com>
2018-03-31 11:16:21 -0600

    Complete storage add tests

--- src/adapters/Storage.js
+++ src/adapters/Storage.js
@@ -1,10 +1,48 @@
+const findVideoIndex = (store, id) => store.videos.findIndex(video => video.id === id)
+
 class Storage {
   constructor() {
     this.namespace = 'modicum'
+    this.initModicum()
+  }
+
+  getStore() {
+    return JSON.parse(localStorage.getItem(this.namespace))
+  }
+
+  setStore(data) {
+    localStorage.setItem(this.namespace, JSON.stringify(data))
+  }
+
+  initModicum() {
+    const defaultSetup = JSON.stringify({ videos: [] })
+    localStorage.getItem(this.namespace) || localStorage.setItem(this.namespace, defaultSetup)
   }
 
   addMove(move) {
-    
+    const store = this.getStore()
+    const videoIndex = findVideoIndex(store, move.videoId)
+
+    if (videoIndex >= 0) {
+      store.videos[videoIndex].moves.push({
+        startTime: move.startTime,
+        name: move.name
+      })
+    } else {
+      const formattedMove = {
+        id: move.videoId,
+        moves: [
+          {
+            startTime: move.startTime,
+            name: move.name
+          }
+        ]
+      }
+
+      store.videos.push(formattedMove)
+    }
+
+    this.setStore(store)
   }
 
 }
--- src/adapters/Storage.test.js
+++ src/adapters/Storage.test.js
@@ -1,9 +1,30 @@
 import Storage from './Storage'
 
 describe('Storage', () => {
+  afterEach(() => { localStorage.clear() })
+
   describe('default setup', () => {
-    it('creates modicum object if it doesnt exist')
-    it('doesnt overwrite pre-existing')
+    it('creates modicum object if it doesnt exist', () => {
+      expect(localStorage.getItem('modicum')).toBeNull
+
+      new Storage()
+
+      const expected = JSON.stringify({ videos: [] })
+
+      expect(localStorage.getItem('modicum')).toEqual(expected)
+    })
+
+    it('doesnt overwrite pre-existing', () => {
+      const preExistingData = JSON.stringify( { videos: [ 'meow' ] } )
+      localStorage.setItem('modicum', preExistingData)
+
+      new Storage()
+
+      const expected = JSON.stringify({ videos: [ 'meow' ] })
+
+      expect(localStorage.getItem('modicum')).toEqual(expected)
+
+    })
   })
 
   describe('add', () => {
@@ -26,8 +47,7 @@ describe('Storage', () => {
             moves: [
               {
                 startTime: move.startTime,
-                name: move.name,
-                videoId: move.videoId
+                name: move.name
               } 
             ]
           }
@@ -36,7 +56,35 @@ describe('Storage', () => {
 
       expect(output).toEqual(expected)
     })
+
+    it('adds second move to existing video', () => {
+      const move = {
+        startTime: '8.382',
+        name: 'meow move',
+        videoId: 'meow'
+      }
+      const storage = new Storage()
+
+      storage.addMove(move)
+
+      const secondMove = {
+        startTime: '3.333',
+        name: 'second move',
+        videoId: 'meow'
+      }
+
+      storage.addMove(secondMove)
+
+      const actual = JSON.parse(localStorage.getItem('modicum'))
+      const expected = {
+        startTime: secondMove.startTime,
+        name: secondMove.name
+      }
+
+      expect(actual.videos.length).toEqual(1)
+      expect(actual.videos[0].moves.length).toEqual(2)
+      expect(actual.videos[0].moves[1]).toEqual(expected)
+    })
   })
 
-  it('adds second move to existing video')
 })
