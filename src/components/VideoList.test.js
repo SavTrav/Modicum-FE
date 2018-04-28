a3f0ab189fe328a4959148d476b20d6c71334da9
Lee Saville <l.w.saville@gmail.com>
2018-04-27 20:10:50 -0600

    Rename MoveList test to VideoList test Fix tests

--- src/components/VideoList.test.js
+++ src/components/VideoList.test.js
@@ -4,26 +4,32 @@ import VideoList from './VideoList'
 
 describe('VideoList', () => {
   const moves = [
-    { startTime: 'time1', name: 'twirly', videoId: 'abc123' },
-    { startTime: 'time2', name: 'spinny', videoId: 'xyz456' },
+    { startTime: 'time1', name: 'twirly' },
+    { startTime: 'time2', name: 'spinny' },
   ]
 
   const defaultProps = {
+    videos: [
+      {
+        id: '123',
+        name: 'fancyterry',
+        moves: moves,
+      }
+    ],
     targetedMove: { name: 'twirly' },
     targetMove: () => {},
-    moves,
   }
 
   it('renders an entire list of moves that is passed in', () => {
     const wrapper = shallow(<VideoList {...defaultProps} />)
-    const texts = wrapper.find('li').map(node => node.text())
+    const texts = wrapper.find('.moves').map(node => node.text())
 
-    expect(texts).toEqual(['Name: twirly, Start time: time1, Video Id: abc123', 'Name: spinny, Start time: time2, Video Id: xyz456'])
+    expect(texts).toEqual(['Name: twirly - startTime: time1', 'Name: spinny - startTime: time2'])
   })
 
   it('highlights the active move with a pretty color', () => {
     const wrapper = shallow(<VideoList {...defaultProps} />)
-    const styles = wrapper.find('li').map(node => node.props().style)
+    const styles = wrapper.find('.moves').map(node => node.props().style)
 
     expect(styles[0]).toEqual({ backgroundColor: 'tomato' })
     expect(styles[1]).toEqual({})
@@ -33,8 +39,8 @@ describe('VideoList', () => {
     const mockFn = jest.fn()
     const wrapper = shallow(<VideoList {...defaultProps} targetMove={mockFn} />)
 
-    wrapper.find('li').first().simulate('click')
+    wrapper.find('.moves').first().simulate('click')
 
-    expect(mockFn).toHaveBeenCalledWith({ startTime: 'time1', name: 'twirly', videoId: 'abc123' })
+    expect(mockFn).toHaveBeenCalledWith('123', { startTime: 'time1', name: 'twirly' })
   })
 })
